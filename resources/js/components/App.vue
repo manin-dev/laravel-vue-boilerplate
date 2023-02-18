80dfb35ab9393e661de0b97c0b0e809e12e2aa1d
manin-dev <chievmanin@gmail.com>
2023-02-18 11:17:15 +0700

    Update Appjs

---
+++ resources/js/components/App.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <h1 class="text-3xl font-bold underline text-center">
+            Laravel 10 , Vue.js 3 , Tailwind CSS and Vite
+        </h1>
+    </div>
+</template>
+<script setup>
+</script>
